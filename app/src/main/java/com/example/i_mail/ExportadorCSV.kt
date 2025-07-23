eebe6fc838567cb42538a1f63bdc54cca3aabfbb
zaulem <samuel.saenz.trabajo@gmail.com>
2025-07-22 20:28:36 -0500

    Arreglando detalles menores en el diseño

--- app/src/main/java/com/example/i_mail/ExportadorCSV.kt
+++ app/src/main/java/com/example/i_mail/ExportadorCSV.kt
@@ -45,7 +45,7 @@ object ExportadorCSV {
             addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
         }
 
-        context.startActivity(Intent.createChooser(intent, "Enviar Docuemento por..."))
+        context.startActivity(Intent.createChooser(intent, "Enviar Documento por..."))
     }
 
     fun exportarArticulosExtendido(context: Context, articulos: List<ArticuloExportado>): File? {
